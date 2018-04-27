f60766a42476200d7358fb1dcfdf7094dbc6efba
Yinbo <pierrecurie@gmail.com>
2018-04-27 01:18:27 -0700

    mostly done with feat ext

--- src/baseline pipe/1_feat_extract.py
+++ src/baseline pipe/1_feat_extract.py
@@ -1,19 +1,30 @@
 import pandas as pd
 import numpy as np
 import wfdb
+from wfdb import processing
+from scipy.stats import kurtosis as kurt, skew
 import pickle
 
 dirName = 'data/training2017/'
-
-x = np.load(dirName + 'train')
-y = np.load(dirName + 'trainlabel')
-
-
+x = np.load(dirName + 'test.npy')
+feats=[]
+for row in x:
+    feats.append(genFeat(row, 300))
+np.save(dirName + 'testFeat', np.array(feats))
 
 def genFeat(signal, fs):
     '''
-    chunk signal into chunks of size chunkSize
-    Input: signal: nonempty 1D np array or list, y: label, chunkSize: int>0
-    Output: 2D numpy array of chunked signal and its label
+    given signal, return list of features
+    Input: signal: nonempty 1D np array, fs: sampling rate, int>0
+    Output: 1D np array
     '''
-
+    idx = processing.xqrs_detect(signal, fs, verbose=False)
+    if len(idx)<2:
+        return np.zeros(11)
+    delta = idx[1:]-idx[:-1]
+    out = [delta.mean(), delta.std(), delta.var(),
+        skew(delta), kurt(delta), delta.max()-delta.min()]
+    rVals=signal[idx]
+    out.extend([rVals.std(), rVals.var(), skew(rVals),
+        kurt(rVals), rVals.max()-rVals.min()])
+    return np.array(out)
